7f30b8e0c0b427bdc017b24903b7f9d237188f0f
Emi <89019222+NalbandyanE@users.noreply.github.com>
2023-08-04 12:25:17 +0400

    Update commandvalidator.h

--- include/commandvalidator.h
+++ include/commandvalidator.h
@@ -8,20 +8,20 @@
 
 class CommandValidator {
 public:
-    CommandValidator();
-    CommandValidator(const Command&);
-    bool isValidCommand(const Command&);
-    Command getCommand() const;
+        CommandValidator();
+        CommandValidator(const Command&);
+        bool isValidCommand(const Command&);
+        Command getCommand() const;
 private:
-    Command command;
-    std::vector<std::string> validCommands = {
-      "CREATE TABLE",
-      "CREATE DATABASE",
-      "SELECT",
-      "INSERT INTO",
-      "UPDATE",
-      "DELETE"  
-    };
+        Command command;
+        std::vector<std::string> validCommands = {
+          "CREATE TABLE",
+          "CREATE DATABASE",
+          "SELECT",
+          "INSERT INTO",
+          "UPDATE",
+          "DELETE"  
+        };
 };
 
 #endif // COMMANDVALIDATOR_H
